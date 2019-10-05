8813a85d7ae4b2f1c8f4a684b8f9be0283940456
Malikovich <md.malikovich@gmail.com>
2019-10-05 17:12:08 +0600

    +image

--- app/src/main/java/com/taskapp/onboard/BoardFragment.java
+++ app/src/main/java/com/taskapp/onboard/BoardFragment.java
@@ -19,7 +19,8 @@ public class BoardFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_board, container, false);
+        View view = inflater.inflate(R.layout.fragment_board, container, false);
+
+        return view;
     }
 }
